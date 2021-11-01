cd2f1b5a8d8c47e82f5e530b0cb4092c0a42712d
Davinder <66485246+shersingh7@users.noreply.github.com>
2021-11-01 15:19:41 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# ## Functions
+# def welcome_message():
+#     print("Hello")
+#     print("Welcome")
+#
+# welcome_message()
+#
+# ## Function with parameters
+# def welcome_message(name):
+#     print("Hello")
+#     print("Welcome")
+#     print(name)
+#
+# welcome_message("DV")
+
+
+## Function returns value
+
+# def square(number):
+#     return number*number
+# print(square(3))
+
+
+
